241bae99c203a045da0397b8673f1d509ce9fdad
AviChristopher <94215685+AviChristopher@users.noreply.github.com>
2022-06-13 13:52:15 +0300

    Update search-list.js

--- search-list.js
+++ search-list.js
@@ -1,11 +1,12 @@
 const inStock = ['garlic', 'coriander', 'eggs', 'apples', 'milk', 'cheese', 'bread', 'dill', 'carrots', 'yogurt', 'potatoes', 'crackers', 'onions', 'tofu', 'lemons', 'cucumbers'];
 const search = prompt('Search for a product.');
 let message;
-
-if (inStock.includes(search) ) {
-message=`Yes, we have <strong>${search}</strong> in the house`}
+if(!search) {
+  message= `<strong>In stock:</strong> ${inStock.join(', ')} `;
+}else if (inStock.includes(search.toLowerCase()) ) {
+message=`Yes, we have <strong>${search.toLowerCase()}</strong> in the house`}
 else {
-message=`No we do not have <strong>${search}</strong> . Please add to grocery list`
+message=`No we do not have <strong>${search.toLowerCase()}</strong> . Please add to grocery list`
 }
 
 document.querySelector('main').innerHTML = `<p>${message} </p>`
